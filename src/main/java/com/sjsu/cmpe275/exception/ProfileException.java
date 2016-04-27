0b5995fd7895b1504069e4bfc73cec201b29b2ee
chenglongwei <weichenglong@gmail.com>
2016-04-26 20:59:40 -0700

    Exception with http code

--- src/main/java/com/sjsu/cmpe275/exception/ProfileException.java
+++ src/main/java/com/sjsu/cmpe275/exception/ProfileException.java
@@ -1,5 +1,8 @@
 package com.sjsu.cmpe275.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author chenglongwei
  * @version 1.2
@@ -7,10 +10,12 @@ package com.sjsu.cmpe275.exception;
  * When GET/DELETE a nonexistence user profile, ProfileException will be thrown and a page will
  * show the exceptionMsg.
  */
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="Sorry, the requested user with ID does not exist.")  // 404
 public class ProfileException extends RuntimeException {
     private String exceptionMsg;
 
     public ProfileException(String exceptionMsg) {
+        super(exceptionMsg);
         this.exceptionMsg = exceptionMsg;
     }
 
--- src/main/webapp/WEB-INF/views/404.jsp
+++ src/main/webapp/WEB-INF/views/404.jsp
@@ -1,3 +1,15 @@
-Page not exist.
-<br>
-<a href="/"> Create Profile Page </a>
+<%--This directive causes the JSP compiler to generate the exception instance variable.
+Otherwise an exception from a DELETE/PUT will be failed, since JSPs only permit GET POST or HEAD --%>
+<%@ page isErrorPage="true" %>
+<html>
+<head>
+    <title>User Profile Exception Handling</title>
+</head>
+<body>
+
+<h2>404</h2>
+
+<h3>Sorry, the requested user with ID does not exist.</h3>
+
+</body>
+</html>
